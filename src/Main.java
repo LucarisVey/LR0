e4f2446d807312d4b99d6bf4d1e44d329c29ac7f
LucarisVey <ruslanp756@gmail.com>
2023-02-22 17:46:47 +0200

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -6,35 +6,37 @@ public class Main {
         Main main = new Main();
         main.run();
     }
-    private double calcY(double a, double b, double x) {
-        return (Math.pow(Math.sin(Math.pow((Math.pow(x,2)+a),2)),3)-Math.sqrt(x/b));
+    private double calcY(double x, double y, double z) {
+        return (Math.pow(Math.sin(Math.pow((Math.pow(z,2)+x),2)),3)-Math.sqrt(z/y));
     }
-    private double calcZ(double a, double b, double x) {
-        return (Math.pow(x, 2)/a)+(Math.cos(Math.pow((x+b),3)));
+    private double calcZ(double x, double y, double z) {
+        return (Math.pow(z, 2)/x)+(Math.cos(Math.pow((z+y),3)));
     }
-    private void print(double a, double b, double x, double y, double z) {
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+    private void print(double x, double y, double z, double a, double b) {
         System.out.println("x = " + x);
-        System.out.println("////////////////////////////////////////////");
         System.out.println("y = " + y);
         System.out.println("z = " + z);
+        System.out.println("////////////////////////////////////////////");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("////////////////////////////////////////////");
+        System.out.println("Hello, World!");
     }
     private void printDate(Date date){
         System.out.println("////////////////////////////////////////////");
         System.out.printf("%1$tA %1$te %1$tB\n", date);
     }
     private void run() {
-        System.out.println("Введіть число a:");
-        Scanner s = new Scanner(System.in);
-        double a = s.nextDouble();
-        System.out.println("Введіть число b:");
-        double b = s.nextDouble();
         System.out.println("Введіть число x:");
+        Scanner s = new Scanner(System.in);
         double x = s.nextDouble();
-        double y = calcY(a, b, x);
-        double z = calcZ(a, b, x);
-        print(a, b, x, y, z);
+        System.out.println("Введіть число y:");
+        double y = s.nextDouble();
+        System.out.println("Введіть число z:");
+        double z = s.nextDouble();
+        double a = calcY(x, y, z);
+        double b = calcZ(x, y, z);
+        print(x, y, z, a, b);
 
         Date date = new Date();
         printDate(date);
